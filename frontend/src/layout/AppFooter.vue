4bc84c90bef5b698448f336565bd1ae96c1b0af0
having-Tech <david.jihoon92@gmail.com>
2019-05-14 16:44:41 +0900

    Delete and Modified

---
+++ frontend/src/layout/AppFooter.vue
@@ -0,0 +1,53 @@
+<template>
+    <footer class="footer has-cards">       
+        <div class="container">
+            <div class="row row-grid align-items-center my-md">
+                <div class="col-lg-6">
+                    <h3 class="text-primary font-weight-light mb-2">Thank you for supporting us!</h3>
+                </div>
+            </div>
+            <hr>
+
+            <div class="row align-items-center justify-content-md-between">
+                <div class="col-md-6">
+                    <div class="copyright">
+                        &copy; {{year}}
+                        <a href="#" target="_blank" rel="noopener">IDOL</a> 
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <ul class="nav nav-footer justify-content-end">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" target="_blank">test Tim</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" target="_blank">About
+                                Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" target="_blank">test</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="https://github.com/creativetimofficial/argon-design-system/blob/master/LICENSE.md"
+                               class="nav-link" target="_blank">MIT License</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'app-footer',
+  data() {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+};
+</script>
+<style>
+</style>
---
+++ frontend/src/router.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import Router from "vue-router";
+import AppHeader from "./layout/AppHeader";
+import AppFooter from "./layout/AppFooter";
+import MainPage from "./views/MainPage.vue";
+
+
+Vue.use(Router);
+
+export default new Router({
+  linkExactActiveClass: "active",
+  routes: [
+    {
+      path: "/",
+      name: "mainpage",
+      components: {
+        header: AppHeader,
+        default: MainPage,
+        footer: AppFooter
+      }
+    }
+  ],
+
+  scrollBehavior: to => {
+    if (to.hash) {
+      return { selector: to.hash };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
+});
